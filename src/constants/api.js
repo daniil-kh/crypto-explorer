3e572467e770bfb01ffa640feda9e6e91d4f91b6
daniil-kh <daniil.khmurovich@gmail.com>
2021-03-09 22:11:44 +0300

    add new url to fetching

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,4 @@
 export const apiUrl = 'https://api.coingecko.com/api/v3/';
 export const coinsUrl = `${apiUrl}coins/`;
 export const marketCoinsUrl = `${coinsUrl}markets/`;
+export const coinDetailUrl = (id) => `${coinsUrl}${id}`;
