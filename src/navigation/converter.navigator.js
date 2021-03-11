da88e6fa971cf152a5af06dddd036fe14acf263f
daniil-kh <daniil.khmurovich@gmail.com>
2021-03-11 18:57:19 +0300

    add preloading logic and new nested navigator

--- src/navigation/converter.navigator.js
+++ src/navigation/converter.navigator.js
@@ -2,10 +2,17 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import ConverterScreen from '../screens/converter/converter.screen';
+import CoinsLoader from '../components/coins-loader/coins-loader';
 
 import COLORS from '../constants/colors';
 
 const Stack = createStackNavigator();
+
+const ConverterScreenWithSpinner = () => (
+  <CoinsLoader>
+    <ConverterScreen />
+  </CoinsLoader>
+);
 
 const ConverterNavigator = () => (
   <Stack.Navigator
@@ -19,7 +26,7 @@ const ConverterNavigator = () => (
     }}>
     <Stack.Screen
       name="ConverterUtil"
-      component={ConverterScreen}
+      component={ConverterScreenWithSpinner}
       options={{title: 'Converter'}}
     />
   </Stack.Navigator>
--- src/navigation/homeStack.navigator.js
+++ src/navigation/homeStack.navigator.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import HomeNavigator from './home.navigator';
-import CoinDetailsScreen from '../screens/coin-details/coin-details.screen';
 import CoinsLoader from '../components/coins-loader/coins-loader';
 import CoinDetailsHeader from '../components/coin-details-header/coin-details-header';
+import CoinsCurrency from '../screens/coins-currency/coins-currency.screen';
+import CoinDetailsInnerNavigator from './coin-detail-inner.navigator';
+import IconButton from '../components/icon-button/icon-button';
+
+import {
+  GetCoinExchangeRateStart,
+  LoadCoinDetailStart,
+} from '../redux/coins/coins.actions';
 
 const CoinDetailsScreenWithLoader = () => (
   <CoinsLoader>
-    <CoinDetailsScreen />
+    <CoinDetailsInnerNavigator />
   </CoinsLoader>
 );
 
@@ -16,37 +24,61 @@ import COLORS from '../constants/colors';
 
 const Stack = createStackNavigator();
 
-const HomeStackNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: COLORS.primaryAqua,
-      },
-    }}>
-    <Stack.Screen
-      options={{headerShown: false}}
-      name="Home"
-      component={HomeNavigator}
-    />
-    <Stack.Screen
-      name="CoinDetail"
-      component={CoinDetailsScreenWithLoader}
-      options={({route}) => {
-        const {shortName, fullName, imageUrl} = route.params;
+const HomeStackNavigator = () => {
+  const dispatch = useDispatch();
 
-        return {
-          headerTitle: () => (
-            <CoinDetailsHeader
-              fullName={fullName}
-              shortName={shortName}
-              imageUrl={imageUrl}
-            />
-          ),
-        };
-      }}
-    />
-    <Stack.Screen name="CoinsCurrencyList" component={} />
-  </Stack.Navigator>
-);
+  useEffect(() => {
+    dispatch(GetCoinExchangeRateStart('bitcoin'));
+  }, []);
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: COLORS.primaryAqua,
+        },
+      }}>
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="Home"
+        component={HomeNavigator}
+      />
+      <Stack.Screen
+        name="CoinDetail"
+        component={CoinDetailsScreenWithLoader}
+        options={({route}) => {
+          const {shortName, fullName, imageUrl, id} = route.params;
+
+          return {
+            headerTitle: () => (
+              <CoinDetailsHeader
+                fullName={fullName}
+                shortName={shortName}
+                imageUrl={imageUrl}
+              />
+            ),
+            headerRight: () => (
+              <IconButton
+                name="ios-refresh"
+                onPress={() => dispatch(LoadCoinDetailStart(id))}
+                size={30}
+                style={{
+                  marginRight: 5,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              />
+            ),
+          };
+        }}
+      />
+      <Stack.Screen
+        name="CoinsCurrencyList"
+        component={CoinsCurrency}
+        options={{title: 'Select Coin'}}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default HomeStackNavigator;
