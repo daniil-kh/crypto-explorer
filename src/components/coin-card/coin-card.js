61363fe35d6b20431ddf43ac9ad21618a0bc0977
daniil-kh <daniil.khmurovich@gmail.com>
2021-03-11 09:37:21 +0300

    styling coin card for better layouting

--- src/components/coin-card/coin-card.js
+++ src/components/coin-card/coin-card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, View} from 'react-native';
 
 import Card from '../card/card';
 import Logo from '../logo/logo';
@@ -11,32 +11,34 @@ import styles from './styles';
 
 const CoinCard = ({coinData, onPress}) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Card
-        style={{
-          width: '100%',
-          marginVertical: 5,
-          height: 60,
-        }}>
-        <Logo imageUrl={coinData.image} style={styles.contentContainer} />
-        <DefaultText
-          text={
-            coinData.name.length > 10
-              ? coinData.symbol.toUpperCase()
-              : coinData.name
-          }
-          styleContainer={[styles.contentContainer]}
-        />
-        <PriceText
-          text={coinData.current_price}
-          styleContainer={styles.contentContainer}
-        />
-        <PercentageText
-          text={coinData.price_change_percentage_1h_in_currency}
-          styleContainer={styles.contentContainer}
-        />
-      </Card>
-    </TouchableOpacity>
+    <View style={{flex: 1}}>
+      <TouchableOpacity onPress={onPress} style={styles.innerTouchable}>
+        <Card
+          style={{
+            width: '100%',
+            marginVertical: 5,
+            height: 60,
+          }}>
+          <Logo imageUrl={coinData.image} style={styles.contentContainer} />
+          <DefaultText
+            text={
+              coinData.name.length > 10
+                ? coinData.symbol.toUpperCase()
+                : coinData.name
+            }
+            styleContainer={[styles.contentContainer]}
+          />
+          <PriceText
+            text={coinData.current_price}
+            styleContainer={styles.contentContainer}
+          />
+          <PercentageText
+            text={coinData.price_change_percentage_1h_in_currency}
+            styleContainer={styles.contentContainer}
+          />
+        </Card>
+      </TouchableOpacity>
+    </View>
   );
 };
 
